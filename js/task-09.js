1e6ff20d21f6396404590560f23854b53d05e832
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-10-18 16:35:44 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const colorBtn = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  colorName.innerHTML = getRandomHexColor();
+}
+
+colorBtn.addEventListener('click', changeColor);
