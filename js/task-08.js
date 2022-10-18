a3c5a5967e06b2496a947fa3fcae914aa2bbd3c6
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-10-18 16:29:39 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+function formSubmit(event) {
+    event.preventDefault();
+
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (!email.value || !password.value) {
+        alert("Усі поля повинні бути заповнені!")
+    } else {
+        let formData = {
+            email: email.value,
+            password: password.value
+        }
+        console.log(formData);
+        event.currentTarget.reset();
+    }
+}
+
+form.addEventListener('submit', formSubmit)
